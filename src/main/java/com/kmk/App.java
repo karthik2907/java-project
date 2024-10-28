e666f7ed9189a2e6a4b7a2b9b4d950511caf1ebb
Colt Steele <colt@gmail.co>
2024-10-28 19:48:56 +0530

    First commit

---
+++ src/main/java/com/kmk/App.java
@@ -0,0 +1,13 @@
+package com.kmk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
